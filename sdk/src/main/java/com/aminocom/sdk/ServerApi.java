00316c942694cbe512511c831c02b218e3ebf294
Aleksei Glazyrin <aleksei.glazyrin@aminocom.com>
2018-10-15 17:10:14 +0300

    Add channel mock model

--- sdk/src/main/java/com/aminocom/sdk/ServerApi.java
+++ sdk/src/main/java/com/aminocom/sdk/ServerApi.java
@@ -1,4 +1,15 @@
 package com.aminocom.sdk;
 
-public interface ServerApi {
+import com.aminocom.sdk.model.network.ChannelResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+interface ServerApi {
+    // TODO: Add service parameter to interceptor
+    @GET("api/user/{user}/recording/search?sort=start_date__desc")
+    Single<ChannelResponse> getChannels(@Path("user") String user,
+                                        @Query("service") String service);
 }
