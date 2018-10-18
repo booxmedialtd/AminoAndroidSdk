f4ccbd288c57b78940c8c330a5c65ec8bf06bfe2
aleksei-amino <aleksei.glazyrin@aminocom.com>
2018-10-18 16:01:55 +0300

    Add cache stubs

--- sdk/src/main/java/com/aminocom/sdk/CacheRepository.java
+++ sdk/src/main/java/com/aminocom/sdk/CacheRepository.java
@@ -1,6 +1,9 @@
 package com.aminocom.sdk;
 
 import com.aminocom.sdk.model.client.Channel;
+import com.aminocom.sdk.model.client.Epg;
+import com.aminocom.sdk.model.client.Group;
+import com.aminocom.sdk.model.client.Program;
 
 import java.util.List;
 
@@ -8,6 +11,9 @@ import io.reactivex.Observable;
 
 class CacheRepository implements LocalRepository {
     private ObservableList<Channel> channels = new ObservableList<>();
+    private ObservableList<Program> programs = new ObservableList<>();
+    private ObservableList<Epg> epgList = new ObservableList<>();
+    private ObservableList<Group> groups = new ObservableList<>();
 
     @Override
     public Observable<List<Channel>> getChannels() {
@@ -17,5 +23,35 @@ class CacheRepository implements LocalRepository {
     @Override
     public void cacheChannels(List<Channel> channels) {
         this.channels.setItems(channels);
+    }
+
+    @Override
+    public Observable<List<Program>> getPrograms() {
+        return programs.getObservable();
+    }
+
+    @Override
+    public void cachePrograms(List<Program> programs) {
+        this.programs.setItems(programs);
+    }
+
+    @Override
+    public Observable<List<Epg>> getEpg() {
+        return epgList.getObservable();
+    }
+
+    @Override
+    public void cacheEpg(List<Epg> epgList) {
+        this.epgList.setItems(epgList);
+    }
+
+    @Override
+    public Observable<List<Group>> getGroup() {
+        return groups.getObservable();
+    }
+
+    @Override
+    public void cacheGroups(List<Group> groups) {
+        this.groups.setItems(groups);
     }
 }
--- sdk/src/main/java/com/aminocom/sdk/LocalRepository.java
+++ sdk/src/main/java/com/aminocom/sdk/LocalRepository.java
@@ -1,6 +1,9 @@
 package com.aminocom.sdk;
 
 import com.aminocom.sdk.model.client.Channel;
+import com.aminocom.sdk.model.client.Epg;
+import com.aminocom.sdk.model.client.Group;
+import com.aminocom.sdk.model.client.Program;
 
 import java.util.List;
 
@@ -10,4 +13,16 @@ public interface LocalRepository {
     Observable<List<Channel>> getChannels();
 
     void cacheChannels(List<Channel> channels);
+
+    Observable<List<Program>> getPrograms();
+
+    void cachePrograms(List<Program> programs);
+
+    Observable<List<Epg>> getEpg();
+
+    void cacheEpg(List<Epg> epgList);
+
+    Observable<List<Group>> getGroup();
+
+    void cacheGroups(List<Group> groups);
 }
