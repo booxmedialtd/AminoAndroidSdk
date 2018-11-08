be29c0b12800a31965484359cdfde37ae0d6a03c
aleksei-amino <aleksei.glazyrin@aminocom.com>
2018-11-08 16:05:28 +0200

    Add live and category fragments

---
+++ demo/src/main/java/com/aminocom/aminoandroidsdk/category/CategoryFragment.java
@@ -0,0 +1,6 @@
+package com.aminocom.aminoandroidsdk.category;
+
+import android.support.v4.app.Fragment;
+
+public class CategoryFragment extends Fragment {
+}
---
+++ demo/src/main/java/com/aminocom/aminoandroidsdk/livetv/LiveTvFragment.java
@@ -0,0 +1,6 @@
+package com.aminocom.aminoandroidsdk.livetv;
+
+import android.support.v4.app.Fragment;
+
+public class LiveTvFragment extends Fragment {
+}
