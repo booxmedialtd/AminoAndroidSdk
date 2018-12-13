8a534014356269f9819d24a9b28ed7c250856feb
aleksei-amino <aleksei.glazyrin@aminocom.com>
2018-12-13 17:19:05 +0200

    Remove unused tests

--- sdk/src/test/java/com/aminocom/sdk/provider/amino/CategoryProviderImplTest.java
+++ sdk/src/test/java/com/aminocom/sdk/provider/amino/CategoryProviderImplTest.java
@@ -5,7 +5,6 @@ import com.aminocom.sdk.Sdk;
 import com.aminocom.sdk.TestCookieManager;
 import com.aminocom.sdk.TestLocalRepository;
 import com.aminocom.sdk.model.client.Category;
-import com.aminocom.sdk.model.client.channel.Channel;
 import com.aminocom.sdk.model.network.UserResponse;
 import com.aminocom.sdk.provider.ProviderType;
 
@@ -19,8 +18,6 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.observers.TestObserver;
 import io.reactivex.subscribers.TestSubscriber;
 import okhttp3.mockwebserver.MockWebServer;
-
-import static org.junit.Assert.assertEquals;
 
 // TODO: check correctness of TestCookieManager
 public class CategoryProviderImplTest {
@@ -45,80 +42,7 @@ public class CategoryProviderImplTest {
 
     // FIXME: Fix mocking of the server
     @Test
-    public void getChannels() throws Exception {
-
-        TestSubscriber<List<Channel>> testObserver = new TestSubscriber<>();
-
-        //String path = "json";
-
-        /*MockResponse mockResponse = new MockResponse()
-                .setResponseCode(200)
-                .setBody(jsonReader.getJson("json/channel_response_4_items.json"));*/
-
-        //mockServer.enqueue(mockResponse);
-
-        sdk.channels().getChannels().subscribe(testObserver);
-        testObserver.awaitTerminalEvent(2, TimeUnit.SECONDS);
-
-        testObserver.assertNoErrors();
-        testObserver.assertValueCount(1);
-
-        //RecordedRequest request = mockServer.takeRequest();
-
-        //assertEquals(path, request.getPath());
-    }
-
-    // FIXME: Fix mocking of the server
-    @Test
-    public void loginCorrect() throws Exception {
-
-        TestObserver<UserResponse> testObserver = new TestObserver<>();
-
-        String user = "aleksei@test.com";
-        String password = "1234";
-
-        /*MockResponse mockResponse = new MockResponse()
-                .setResponseCode(200)
-                .setBody(getJson("json/login_response.json"));
-
-        mockServer.enqueue(mockResponse);*/
-
-        sdk.user().login(user, password).subscribe(testObserver);
-        testObserver.awaitTerminalEvent(2, TimeUnit.SECONDS);
-
-        testObserver.assertNoErrors();
-        testObserver.assertValueCount(1);
-    }
-
-    // FIXME: Fix mocking of the server
-    @Test
-    public void loginWrong() throws Exception {
-        Sdk sdk = new Sdk(
-                "https://nebtest1.auto.neb.amo.booxmedia.xyz/",
-                "mobileclient",
-                "qn05BON1hXGCUsw",
-                ProviderType.AMINO,
-                new TestCookieManager(),
-                new TestLocalRepository());
-
-        TestObserver<UserResponse> testObserver = new TestObserver<>();
-
-        String user = "some_user@test.com";
-        String password = "0000";
-
-        //MockResponse mockResponse = new MockResponse().setResponseCode(401);
-
-        //mockServer.enqueue(mockResponse);
-
-        sdk.user().login(user, password).subscribe(testObserver);
-        testObserver.awaitTerminalEvent(2, TimeUnit.SECONDS);
-
-        assertEquals(1, testObserver.errorCount());
-    }
-
-    // FIXME: Fix mocking of the server
-    @Test
-    public void getCategories_NoLogin() throws Exception {
+    public void getCategories_GuestUser() throws Exception {
 
         TestSubscriber<List<Category>> testObserver = new TestSubscriber<>();
 
@@ -131,7 +55,7 @@ public class CategoryProviderImplTest {
 
     // FIXME: Fix mocking of the server.
     @Test
-    public void getCategories_Login() throws Exception {
+    public void getCategories_AuthorizedUser() throws Exception {
 
         TestObserver<UserResponse> loginObserver = new TestObserver<>();
 
