2088788308676c9cf1eefe4beab0bc3a76aef3de
aleksei-amino <aleksei.glazyrin@aminocom.com>
2018-12-31 10:42:17 +0200

    Add favorite recording endpoint

--- sdk/src/main/java/com/aminocom/sdk/ServerApi.java
+++ sdk/src/main/java/com/aminocom/sdk/ServerApi.java
@@ -52,6 +52,11 @@ public interface ServerApi {
                                            @Query("st") long startTime,
                                            @Query("et") long endTime);
 
+    @GET("api/user/{user}/recording/favorite?sort=start_date__desc")
+    Single<RecordingResponse> getFavoriteRecording(@Path("user") String user,
+                                           @Query("service") String service,
+                                           @Query("st") long startTime,
+                                           @Query("et") long endTime);
 
     @GET("api/v1/channels/{channelId}/relationships/streams")
     Single<StreamResponse> getChannelStreams(@Path("channelId") String channelId, @Query("service") String service);
